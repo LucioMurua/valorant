cbe2692191ae5db6c222413dc7be4ee21e7035ed
Lucio Murua <luciomurua1992@gmail.com>
2023-07-12 21:59:17 -0300

    maps section started

--- scripts/animations.js
+++ scripts/animations.js
@@ -1,84 +1,113 @@
 gsap.registerPlugin(ScrollTrigger);
 
 function isDesktop() {
-    const mediaQuery = window.matchMedia('(min-width: 1100px)'); // Ajusta el valor según tus necesidades
-    return mediaQuery.matches;
-  }
+  const mediaQuery = window.matchMedia("(min-width: 1100px)"); // Ajusta el valor según tus necesidades
+  return mediaQuery.matches;
+}
 
 if (isDesktop()) {
+  // ============== NEWS ANIMATIONS =============
 
-    
-    // ============== NEWS ANIMATIONS =============
-
-    gsap.fromTo(
-        ".news__background",
-        { y: -110 },
-        {
-            y: 100,
-    scrollTrigger: {
+  gsap.fromTo(
+    ".news__background",
+    { y: -110 },
+    {
+      y: 100,
+      scrollTrigger: {
         trigger: ".news",
         scrub: true,
         // markers:true,
         start: "top bottom",
         end: "bottom top",
-    },
-}
-);
+      },
+    }
+  );
 
-gsap.from(".news__title", {
+  gsap.from(".news__title", {
     y: "100%",
     delay: 0.5,
     scrollTrigger: {
-        trigger: ".news__titleContainer",
-        // markers: true,
-    start: "top bottom",
-  },
-});
+      trigger: ".news__titleContainer",
+      // markers: true,
+      start: "top bottom",
+    },
+  });
 
-const imgTimeLine = gsap.timeline();
+  const imgTimeLine = gsap.timeline();
 
-imgTimeLine
-.from(".slider__img1", { y: 100, opacity: 0, duration: .5, delay: 1})
-.from(".slider__img2", { y: 100, opacity: 0, duration: .5},"<.2")
-.from(".slider__img3", { y: 100, opacity: 0, duration: .5}, "<.2");
+  imgTimeLine
+    .from(".slider__img1", { y: 100, opacity: 0, duration: 0.5, delay: 1 })
+    .from(".slider__img2", { y: 100, opacity: 0, duration: 0.5 }, "<.2")
+    .from(".slider__img3", { y: 100, opacity: 0, duration: 0.5 }, "<.2");
 
-// ============= ACT ANIMATIONS ================
-gsap.from(".act__title", {
+  // ============= ACT ANIMATIONS ================
+  gsap.from(".act__title", {
     y: "100%",
     delay: 0.2,
     scrollTrigger: {
-        trigger: ".act__titleContainer",
-        start: "top bottom",
+      trigger: ".act__titleContainer",
+      start: "top bottom",
     },
-});
+  });
 
-// =============== OVERVIEW ANIMATIONS ================
-gsap.from(".overview__title",{
-    y:200,
-    delay: .2,
-    scrollTrigger:{
-        trigger:".overview__titleContainer",
-        start: "top bottom"
-    }
-})
+  // =============== OVERVIEW ANIMATIONS ================
+  gsap.from(".overview__title", {
+    y: 200,
+    delay: 0.2,
+    scrollTrigger: {
+      trigger: ".overview__titleContainer",
+      start: "top bottom",
+    },
+  });
 
-gsap.from(".protocol__line",{
+  gsap.from(".protocol__line", {
     width: 0,
-    duration: .5,
-    delay: .2,
-    scrollTrigger:{
-        trigger: ".protocol__line"
-    }
-})
+    duration: 0.5,
+    delay: 0.2,
+    scrollTrigger: {
+      trigger: ".protocol__line",
+    },
+  });
 
-gsap.from(".protocol__text",{
-    duration: .5,
+  gsap.from(".protocol__text", {
+    duration: 0.5,
     text: "AKBJH - POIOS - EPPSA",
     ease: "none",
+    delay: 0.2,
+    scrollTrigger: {
+      trigger: ".protocol__text",
+    },
+  });
+
+  // =========== AGENTS ANIMATION ============
+  gsap.fromTo(
+    ".agents__contentBackground",
+    { y: -100 },
+    {
+      y: 50,
+      scrollTrigger: {
+        trigger: ".agents__contentBackground",
+        scrub: 1,
+      },
+    }
+  );
+
+  gsap.fromTo(".agents__models",
+    { y: 50 },
+    {
+      y: -130,
+      scrollTrigger: {
+        trigger: ".agents__models",
+        scrub: 1,
+      },
+    }
+  );
+
+  gsap.from('.agents__title',{
+    y: '100%',
     delay: .2,
     scrollTrigger:{
-        trigger:".protocol__text"
+      trigger:'.agents__titleContainer',
     }
-})
-
+  })
 }
